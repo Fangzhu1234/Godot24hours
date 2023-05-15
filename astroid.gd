c7f8266069da7a0085e68c7082e685ff55950e34
Fangzhu Deng <dengfz@gmail.com>
2023-05-15 14:29:35 -0600

    Add UI and score count Add restart game Add scroll background

--- astroid.gd
+++ astroid.gd
@@ -1,11 +1,11 @@
 extends Area2D
 
 @export var astroid_speed = 250
-signal astroid_explosion(position)
+signal astroid_explosion(position, score)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	astroid_speed = randf_range(100, 300)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,5 +19,5 @@ func _on_visible_on_screen_enabler_2d_screen_exited():
 
 func _on_area_entered(area):
 	if area.is_in_group("bullets") or area.is_in_group("ship"):
-		emit_signal("astroid_explosion", position)
+		emit_signal("astroid_explosion", position, astroid_speed)
 		queue_free()
--- space.gd
+++ space.gd
@@ -7,20 +7,26 @@ var explosion_scene = preload("res://explosion.tscn")
 
 var player
 var spawn_position
-
+var score = 0
+var screen_size = Vector2()
+@export var scroll_speed = 20
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
+	screen_size = get_viewport_rect().size
 	player = ship_scene.instantiate()
 	player.position = Vector2(20, 90)
 	player.connect("ship_fired", _on_ship_fired)
-	player.connect("ship_explosion", _on_explosion)
+	player.connect("ship_explosion", _on_ship_explosion)
 	add_child(player)
 	spawn_position = $Path2D/PathFollow2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if $Background.position.x <= -screen_size.x:
+		$Background.position = Vector2(0, 0)
+	else:
+		$Background.position += Vector2(-scroll_speed * delta, 0)
 	
 
 func _on_ship_fired(fire_position):
@@ -38,7 +44,21 @@ func _on_spawn_timer_timeout():
 	add_child(astroid)
 	$SpawnTimer.wait_time = randf_range(0.5, 3)
 
-func _on_explosion(object_position):
+func _on_explosion(object_position, object_score):
 	var explosion = explosion_scene.instantiate()
 	explosion.position = object_position
 	add_child(explosion)
+	score += object_score
+	$HBoxContainer/ScoreLabel.text = "score: " + str(int(score)) 
+	
+func _on_ship_explosion(ship_position):
+	var explosion = explosion_scene.instantiate()
+	explosion.position = ship_position
+	add_child(explosion)
+	$GameoverLabel.show()
+	$HBoxContainer/ScoreLabel.text = "score: 0"
+	$RestartTimer.start()
+
+func _on_restart_timer_timeout() -> void:
+	print("restart game now")
+	get_tree().change_scene_to_file("res://space.tscn")
